20aae5609d236483665fb57e8abc3b5b0f75f522
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-09-17 15:16:58 +0200

    Added sections_backwards()

--- zim_to_corpus/html.py
+++ zim_to_corpus/html.py
@@ -4,7 +4,9 @@
 """Stuff used for HTML parsing."""
 
 import re
+from typing import Generator, Union
 
+from bs4 import BeautifulSoup
 from bs4.element import Tag
 
 # Pattern for recognizing headers
@@ -20,3 +22,15 @@ def get_title(section: Tag) -> str:
             return child.get_text()
     else:
         raise ValueError('No header in section')
+
+
+def sections_backwards(tree: Union[BeautifulSoup, Tag]) -> Generator[Tag, None, None]:
+    """Enumerates the sections in a document or HTML tree in reverse order."""
+    if isinstance(tree, BeautifulSoup):
+        yield from sections_backwards(tree.body)
+    for child in tree.contents[::-1]:
+        if isinstance(child, Tag) and child.name == 'section':
+            yield from sections_backwards(child)
+    else:
+        if tree.name == 'section':
+            yield tree
