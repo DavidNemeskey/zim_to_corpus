69fa0561d9a3b801a71c7801a15bca95fef51975
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-10-03 16:03:52 +0200

    Added code to parse Project Gutenberg zim archives. Still need to wire it into the parsing script, though.

--- zim_to_corpus/readers/__init__.py
+++ zim_to_corpus/readers/__init__.py
@@ -1,2 +1,3 @@
 from .simple_html import parse as parse_simple_html
+from .zim_gutenberg import parse as parse_gutenberg
 from .zim_wiki import parse as parse_zim_wiki, enumerate_static_dump
--- zim_to_corpus/readers/zim_gutenberg.py
+++ zim_to_corpus/readers/zim_gutenberg.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Reader for the Project Gutenberg data in Kiwix's .zim file."""
+"""
+Reader for the Project Gutenberg data in Kiwix's .zim file.
+
+Parses the HTML text of a book from Project Gutenberg to a cleaner and
+simpler HTML structure.
+
+.. warning::
+Note that this code can only parse the HTML in the Kiwix ZIM archives. The
+exact structure, class names, ids, etc. might be different from the HTML
+on the Project Gutenberg page. Also, due to the lack of a unified schema
+for PG books, the parser might drop valid text or include advertisements,
+etc.
+"""
 
 import re
 
 from bs4 import BeautifulSoup
-from bs4.element import Comment, NavigableString, Tag
+from bs4.element import Tag
 
 from zim_to_corpus.html import headerp, listp
 
-class ZimGutenbergParser:
-    """
-    Parses the HTML text of a book from Project Gutenberg to a cleaner and
-    simpler HTML structure.
 
-    .. warning::
-    Note that this code can only parse the HTML in the Kiwix ZIM archives. The
-    exact structure, class names, ids, etc. might be different from the HTML
-    on the Project Gutenberg page. Also, due to the lack of a unified schema
-    for PG books, the parser might drop valid text or include advertisements,
-    etc.
-    """
-    # Template for the output (simplified) html
-    html_template = """<html>
+# Template for the output (simplified) html
+html_template = """<html>
     <head>
         <title></title>
         <meta charset="UTF-8">
@@ -31,68 +32,104 @@ class ZimGutenbergParser:
     <body></body>
 </html>"""
 
-    def __init__(self, html_text: str):
-        self.old_bs = BeautifulSoup(html_text)
-        self.new_bs = BeautifulSoup(self.html_template)
-        self.title = self.old_bs.find('title').get_text()
 
-    def parse_book(self) -> BeautifulSoup:
-        """Parses a whole book."""
-        self.new_bs.html.head.title.append(self.old_bs.find('title').get_text())
+def filter_tree(tree: BeautifulSoup):
+    """Filters page numbers, tables and suchlike."""
+    for pn in tree.find_all('span', {'class': 'pageno'}):
+        pn.decompose()
+    for pn in tree.find_all('span', {'class': 'pagenum'}):
+        pn.decompose()
+    for table in tree.find_all('table'):
+        table.decompose()
 
-        # Let's start with the main content
-        self.filter_tree()
-        tmp_bs = self.pre_parse()
-        return tmp_bs
+def pre_parse(old_bs: BeautifulSoup) -> BeautifulSoup:
+    """
+    Pre-parses the old html and adds headers, lists, paragraphs and poems
+    to the new html.
+    """
+    def add_text(old_tag: Tag, new_tag: Tag, new_parent: Tag):
+        text = ' '.join(old_tag.get_text().split())
+        if text:
+            new_tag.append(text)
+            new_parent.append(new_tag)
 
-    def pre_parse(self) -> BeautifulSoup:
-        """
-        Pre-parses the old html and adds headers, lists, paragraphs and poems
-        to the new html.
-        """
-        old_body = self.old_bs.find('body')
-        tmp_bs = BeautifulSoup(self.html_template)
-        new_body = tmp_bs.html.body
-        for tag in old_body.find_all(re.compile('^(?:p|ul|ol|h[1-5]|div)$')):
-            if tag.name != 'div':
-                new_tag = tmp_bs.new_tag(tag.name)
-                if tag.name == 'p':
-                    new_tag.append(' '.join(tag.get_text().split()))
-                elif headerp.match(tag.name):
-                    new_tag.append(tag.get_text())
-                elif listp.match(tag.name):
-                    for li in tag.children:
-                        if isinstance(li, Tag) and li.name == 'li':
-                            new_li = tmp_bs.new_tag('li')
-                            new_li.append(' '.join(li.get_text().split()))
-                            new_tag.append(new_li)
-                new_body.append(new_tag)
-            else:  # div
-                if 'poem' not in tag.get('class', []):
-                    continue
-                print('Found poem')
-                if tag.find('div', {'class': 'stanza'}):
-                    print('Found stanza')
-                    # One p per stanza
-                    for stanza in tag.find_all('div', {'class': 'stanza'}):
-                        new_p = tmp_bs.new_tag('p')
-                        for line in stanza.find_all('span'):
-                            new_p.append(line.get_text())
-                        new_body.append(new_p)
-                else:
-                    print('No stanza found')
-                    # Unstructured poem with lines as paragraphs
+    old_body = old_bs.find('body')
+    tmp_bs = BeautifulSoup(html_template)
+    new_body = tmp_bs.html.body
+    for tag in old_body.find_all(re.compile('^(?:p|ul|ol|h[1-5]|div)$')):
+        if tag.name != 'div':
+            new_tag = tmp_bs.new_tag(tag.name)
+            if tag.name == 'p':
+                add_text(tag, new_tag, new_body)
+            elif headerp.match(tag.name):
+                add_text(tag, new_tag, new_body)
+            elif listp.match(tag.name):
+                for li in tag.children:
+                    if isinstance(li, Tag) and li.name == 'li':
+                        new_li = tmp_bs.new_tag('li')
+                        add_text(li, new_li, new_tag)
+                if new_tag.contents:
+                    new_body.append(new_tag)
+        else:  # div
+            if 'poem' not in tag.get('class', []):
+                continue
+            if tag.find('div', {'class': 'stanza'}):
+                # One p per stanza
+                for stanza in tag.find_all('div', {'class': 'stanza'}):
                     new_p = tmp_bs.new_tag('p')
-                    for line in tag.find_all('p'):
-                        new_p.append(line.get_text())
+                    for line in stanza.find_all('span'):
+                        new_p.append(line.get_text().strip())
+                    if new_p.contents:
+                        new_body.append(new_p)
+            else:
+                # Unstructured poem with lines as paragraphs
+                new_p = tmp_bs.new_tag('p')
+                for line in tag.find_all('p'):
+                    new_p.append(line.get_text().strip())
+                if new_p.contents:
                     new_body.append(new_p)
-        return tmp_bs
+    return tmp_bs
 
-    def filter_tree(self):
-        """Filters page numbers, tables and suchlike."""
-        for pn in self.old_bs.find_all('span', {'class': 'pageno'}):
-            pn.decompose()
-        for pn in self.old_bs.find_all('span', {'class': 'pagenum'}):
-            pn.decompose()
-        for table in self.old_bs.find_all('table'):
-            table.decompose()
+
+def add_sections(old_bs: BeautifulSoup):
+    new_bs = BeautifulSoup(html_template)
+    old_body = old_bs.html.body
+    new_body = new_bs.html.body
+    section = new_bs.new_tag('section')
+    last_added = None
+    # Note: the first 5 lines are a replacement of the proper for loop
+    # commented out below. The reason for this is that when a tag is added
+    # to a section, somehow the children iterator is messed up and as a
+    # result, only every second tag is added to the section. So clumsy
+    # while loop it is
+
+    # for tag in old_body.children:
+    while True:
+        try:
+            tag = next(old_body.children)
+        except StopIteration:
+            break
+        if not isinstance(tag, Tag):
+            print(f'WARNING, not tag: {tag}')
+        if headerp.match(tag.name):
+            if last_added and not headerp.match(last_added):
+                if section.contents:
+                    new_body.append(section)
+                section = new_bs.new_tag('section')
+        section.append(tag)
+        last_added = tag.name
+    if section.contents:
+        new_body.append(section)
+
+    return new_bs
+
+
+def parse(html_text: str) -> BeautifulSoup:
+    """Parses a whole book."""
+    # Let's start with the main content
+    old_bs = BeautifulSoup(html_text)
+    filter_tree(old_bs)
+    tmp_bs = pre_parse(old_bs)
+    new_bs = add_sections(tmp_bs)
+    new_bs.html.head.title.append(old_bs.find('title').get_text())
+    return new_bs
